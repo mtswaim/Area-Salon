8ac74b0fcc13b934faf509f8bd0dfb900698b809
Matthew Swaim <Mtswaim@Matthews-MacBook-Pro-3.local>
2019-11-19 14:11:32 -0500

    styling header

--- src/components/ArtistDetails.jsx
+++ src/components/ArtistDetails.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function ArtistDetails(props) {
   return (
     <div id='artist-box'>
-      <img> src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-thick-hair-gettyimages-1035491002.jpg'</img>
+      <img>src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-thick-hair-gettyimages-1035491002.jpg'</img>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 export default function Header(props) {
 
   return (
-    <header>
-      <h1>Aerea Salon</h1>
-      <h2>52 W 21st St New York, NY 10010 | 212.929.4680 </h2>
-      <button>Book</button>
-      <line></line>
+    <header id='header'>
+      <h1 id='title'>Aerea Salon</h1>
+      <h2 id='address'>52 W 21st St New York, NY 10010 | 212.929.4680 </h2>
+      <button id='book-button'>Book</button>
+      <line id="line"></line>
     </header>
   )
 }
