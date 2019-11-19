478cc82eb5ccccb8ea69a51b2b25754a485a3fb8
Ben Grzybowski <bengrzybowski@Bens-MacBook-Pro.local>
2019-11-19 15:03:37 -0500

    more style on artists

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -11,23 +11,29 @@ export default function Artists() {
   return (
     <div className='artists'>
       <Slider {...settings}>
-        <div>
+        <div className="container">
           <img className="artist-image" src={artist1} alt="none"></img>
+          <div className="centered">Sally</div>
         </div>
-        <div>
+        <div className="container">
           <img className="artist-image" src={artist2} alt="none"></img>
+          <div className="centered">Nancy</div>
         </div>
-        <div>
+        <div className="container">
           <img className="artist-image" src={artist3} alt="none"></img>
+          <div className="centered">Beth</div>
         </div>
-        <div>
+        <div className="container">
           <img className="artist-image" src="https://www.hji.co.uk/wp-content/efs/2018/10/legal-advice-freelance-hairdresser-shutterstock_516922987.jpg" alt="none"></img>
+          <div className="centered">Joan</div>
         </div>
-        <div>
+        <div className="container">
           <img className="artist-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL6QG-djQs0B2c0dEbwXKdtYmsSpzFQsX38YYRcrvZmmR9MJoR&s" alt="none"></img>
+          <div className="centered">Kim</div>
         </div>
-        <div>
+        <div className="container">
           <img className="artist-image" src="https://hairdressingtraining-homestudy.com/images/articles-GDM/large_500px/hairdresser_posture_131278703_j2.jpg" alt="none"></img>
+          <div className="centered">Jan</div>
         </div>
       </Slider>
     </div >
